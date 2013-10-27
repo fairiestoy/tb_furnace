8638d5f53bdb46b356c8a1a8633db93c26a6fe4e
fairiestoy <johannesvk@web.de>
2013-10-27 02:26:34 +0200

    Push to v206608
    Rewritten the code again. Still has a little bug with fuel items, but
    its executable now.

--- README.txt
+++ README.txt
@@ -4,7 +4,7 @@ README
 Time Based Furnaces
 -------------------
 
-Version: 70160
+Version: 206608
 Author: fairiestoy
 
 Description:
@@ -22,3 +22,8 @@ Version History:
 	Initial commit and first basic design
 > 135696
 	Rewritten the code a bit more, but still has this ugly bug
+> 206608
+	A way better rewrite. Put some routines in external
+	functions and made the code more executable. Known
+	Bugs: First fuel item is not consumed, so has a little
+	exploit feeling
--- init.lua
+++ init.lua
@@ -5,11 +5,51 @@ This furnace follows a few different rules then
 the builtin ones. Instead of waiting on ABMs,
 it works with time based calculations.
 
-Version: 135696
+Version: 206608
 Author: fairiestoy
 ]]
 
 tb_furnaces = {}
+
+local function show_formspec( pos, player_name , form_index )
+	local pos_string = pos.x..','..pos.y..','..pos.z
+	local form_table = {
+		"size[9,9]"..
+			"image[3,3;1,1;default_furnace_fire_bg.png]"..
+			"list[nodemeta:"..pos_string..";fuel;1,4;2,2;]"..
+			"list[nodemeta:"..pos_string..";src;1,1;2,2;]"..
+			"list[nodemeta:"..pos_string..";dst;5,1;4,4;]"..
+			"list[current_player;main;0,6;8,4;]",
+		"size[9,9]"..
+			"image[3,3;1,1;default_furnace_fire_bg.png^default_furnace_fire_fg.png]"..
+			"list[nodemeta:"..pos_string..";fuel;1,4;2,2;]"..
+			"list[nodemeta:"..pos_string..";src;1,1;2,2;]"..
+			"list[nodemeta:"..pos_string..";dst;5,1;4,4;]"..
+			"list[current_player;main;0,6;8,4;]",
+		}
+	minetest.show_formspec( player_name, 'tb_furnace:formspec', form_table[ form_index ] )
+end
+
+local function gather_time_information( meta_data )
+	-- Collect time information
+	local current_time = os.time()
+	local last_time = meta_data:get_int( 'src_time' )
+	local src_diff = current_time - last_time
+	last_time = meta_data:get_int( 'fuel_time' )
+	local fuel_diff = current_time - last_time
+	return src_diff, fuel_diff
+end
+
+local function detect_slot_item( inv, slot )
+	local fuellist = inv:get_list( slot )
+	for index = 1, inv:get_size( slot ), 1 do
+		temp_stack = inv:get_stack( slot, index )
+		if not temp_stack:is_empty() then
+			return index
+		end
+	end
+	return 0 -- No item found
+end
 
 tb_furnaces.furnace_is_ready = function( meta, inv, listname, stack )
 	local return_value = {}
@@ -58,153 +98,121 @@ minetest.register_node("tb_furnace:s_furnace", {
 	on_rightclick = function( pos, node, clicker, itemstack )
 		local meta = minetest.get_meta( pos )
 		if meta:get_int( 'cooking_state' ) == 0 then
-			local pos_string = pos.x..','..pos.y..','..pos.z
-			local formspec = "size[9,9]"..
-				"image[3,3;1,1;default_furnace_fire_bg.png]"..
-				"list[nodemeta:"..pos_string..";fuel;1,4;2,2;]"..
-				"list[nodemeta:"..pos_string..";src;1,1;2,2;]"..
-				"list[nodemeta:"..pos_string..";dst;5,1;4,4;]"..
-				"list[current_player;main;0,6;8,4;]"
-			-- Looks like we not have been busy up to now
-			minetest.show_formspec(  clicker:get_player_name() , 'tb_furnace:unused', formspec )
-		else
-			local pos_string = pos.x..','..pos.y..','..pos.z
-			local formspec = "size[9,9]"..
-				"image[3,3;1,1;default_furnace_fire_bg.png^default_furnace_fire_fg.png]"..
-				"list[nodemeta:"..pos_string..";fuel;1,4;2,2;]"..
-				"list[nodemeta:"..pos_string..";src;1,1;2,2;]"..
-				"list[nodemeta:"..pos_string..";dst;5,1;4,4;]"..
-				"list[current_player;main;0,6;8,4;]"
-			-- Collect time information
-			local last_time = meta:get_int( 'src_time' )
-			local inv = meta:get_inventory()
-			local current_time = os.time()
-			local src_diff = current_time - last_time
-			last_time = meta:get_int( 'fuel_time' )
-			local fuel_diff = current_time - last_time
-			if ( not src_diff or src_diff == 0 ) and ( not fuel_diff or fuel_diff == 0 ) then
-				minetest.show_formspec(  clicker:get_player_name() , 'tb_furnace:unused', formspec )
+			show_formspec( pos, clicker:get_player_name(), 1 )
+		elseif meta:get_int( 'cooking_state' ) == 1 then
+			local src_diff, fuel_diff = gather_time_information( meta )
+			if src_diff == 0 and fuel_diff == 0 then
+				show_formspec( pos, clicker:get_player_name(), 2 )
 				return
 			end
-			-- First, gather necessary informations
-			-- src
-			local src_list_results = {}
-			local srclist = inv:get_list("src")
-			local cooked, aftercooked = nil
-			cooked, aftercooked = minetest.get_craft_result({method = "cooking", width = 1, items = srclist})
-			src_list_results['amount'] = math.floor( src_diff / cooked.time )
-			src_list_results['time'] = cooked.time
-			src_list_results['total_time'] = cooked.time * src_list_results['amount']
-			local temp_stack, total_items = nil, 0
-			for index = 1, 4, 1 do
-				temp_stack = inv:get_stack( 'src', index )
-				if not temp_stack:is_empty() then
-					total_items = total_items + temp_stack:get_count()
+			local security_trigger = 0
+			local loop_trigger = true
+			while loop_trigger do
+				-- Since we have more than one slot and therefore
+				-- more than one possible item, iterate through
+				-- the slots
+				local inv = meta:get_inventory()
+				src_index = detect_slot_item( inv, 'src' )
+				fuel_index = detect_slot_item( inv, 'fuel' )
+				if src_index == 0 or fuel_index == 0 then
+					show_formspec( pos, clicker:get_player_name(), 1 )
+					return
 				end
-			end
-			src_list_results['item_amount'] = total_items
-			print( 'src_list_results.item_amount: '..src_list_results.item_amount )
-			print( 'src_list_results.amount: '..src_list_results.amount )
-			if src_list_results.item_amount < src_list_results.amount then
-				-- we don't have enough items, so manipulate items a bit
-				src_list_results.amount = src_list_results.item_amount
-				-- disable cooking due to missing source items
-				print( 'Switched to inactive state...1' )
-				meta:set_int( 'cooking_state', 0 )
-			end
-			print( 'src_list_results.amount after changing: '..src_list_results.amount )
-			-- fuel
-			local fuel_list_results = {}
-			local fuellist = inv:get_list('fuel')
-			local fuel, afterfuel = nil
-			fuel, afterfuel = minetest.get_craft_result({method = "fuel", width = 1, items = fuellist})
-			fuel_list_results['amount'] = math.floor( fuel_diff / fuel.time )
-			fuel_list_results['time'] = fuel.time
-			fuel_list_results['total_time'] = fuel.time * fuel_list_results['amount']
-			temp_stack, total_items = nil, 0
-			for index = 1, 4, 1 do
-				temp_stack = inv:get_stack( 'fuel', index )
-				if not temp_stack:is_empty() then
-					total_items = total_items + temp_stack:get_count()
+				-- material calculations
+				local tmp_list = {
+					{},
+					{}
+				}
+				local map_list = {
+					'src',
+					'fuel',
+					'cooking',
+					'fuel',
+					src_index,
+					fuel_index
+				}
+				for index = 1, 2 ,1 do
+					local mtr_list_results = {}
+					local mtr_list = inv:get_list( map_list[index] )
+					local cooked, aftercooked = minetest.get_craft_result({method = map_list[index + 2], width = 1, items = {mtr_list[map_list[index + 4]]}})
+					mtr_list_results['amount'] = math.floor( src_diff / cooked.time )
+					mtr_list_results['time'] = cooked.time
+					mtr_list_results['total_time'] = cooked.time * mtr_list_results['amount']
+					mtr_list_results['item_amount'] = inv:get_stack( map_list[index], map_list[index + 4] ):get_count()
+					mtr_list_results['item'] = cooked.item
+					mtr_list_results['aitem'] = aftercooked.item
+					if mtr_list_results.item_amount < mtr_list_results.amount then
+						-- we don't have enough items, so manipulate items a bit
+						mtr_list_results.amount = mtr_list_results.item_amount
+						-- disable cooking due to missing source items
+						meta:set_int( 'cooking_state', 0 )
+					end
+					tmp_list[index] = mtr_list_results
 				end
-			end
-			if not fuel.item or fuel.item == nil then
-				print( 'fuel.item could not be detected...' )
-			end
-			fuel_list_results['item_amount'] = total_items
-			print( 'fuel_list_results.item_amount: '..fuel_list_results.item_amount )
-			print( 'fuel_list_results.amount: '..fuel_list_results.amount )
-			if fuel_list_results.item_amount < fuel_list_results.amount then
-				-- we don't have enough items, so manipulate items a bit
-				fuel_list_results.amount = fuel_list_results.item_amount
-				print( 'Switched to inactive state...2' )
-				meta:set_int( 'cooking_state', 0 )
-			end
-			-- dst
-			local dst_list = {}
-			temp_stack, total_items = nil, 0
-			for index = 1, inv:get_size( 'dst' ), 1 do
-				temp_stack = inv:get_stack( 'dst', index )
-				if not temp_stack:is_empty() then
-					total_items = total_items + temp_stack:get_free_space()
-				end
-			end
-			total_items = ( cooked.item:get_stack_max() * inv:get_size( 'dst' ) ) - total_items
-			dst_list['free_space'] = total_items
-			-- Time to see how much results we cooked
-			if fuel_list_results['total_time'] < src_list_results['total_time'] and fuel_list_results.total_time ~= 0 then
-				print( 'Fuel total time lower than src total time? :'..fuel_list_results.total_time..' : '..src_list_results.total_time )
-				-- we didn't had enough fuel to cook all items, correct value
-				src_list_results['total_time'] = fuel_list_results['total_time']
-				src_list_results['amount'] = math.floor( src_list_results['total_time'] / src_list_results['time'] )
-			else
-				-- there was more fuel than needed
-				fuel_list_results['total_time'] = src_list_results['total_time']
-				fuel_list_results['amount'] = math.floor( fuel_list_results['total_time'] / fuel_list_results['time'] )
-			end
-			print( 'Src amount: '..src_list_results['amount']..' Dst place: '..dst_list.free_space )
-			if dst_list.free_space < ( src_list_results['amount'] * cooked.item:get_count() ) then
-				-- we also have to correct the free space amount -_-
-				src_list_results['amount'] = math.floor( total_items / cooked.item:get_count() )
-				src_list_results['total_time'] = src_list_results['amount'] * src_list_results['time']
-				fuel_list_results['total_time'] = src_list_results['total_time']
-				fuel_list_results['amount'] = fuel_list_results['total_time'] * fuel_list_results['time']
-				meta:set_int( 'cooking_state', 0 )
-				print( 'Switched to inactive state...4' )
-			end
-			if ( src_diff > cooked.time ) then
-				temp_stack, total_items = nil, 0
-				total_items = cooked.item:get_count() * src_list_results['amount']
-				temp_stack = cooked.item:to_table()
-				temp_stack.count = total_items
-				temp_stack = ItemStack( temp_stack )
-				inv:add_item( 'dst', temp_stack )
-				temp_stack = aftercooked.items[1]:to_table()
-				if temp_stack == nil then
-					for key, value in pairs( aftercooked.items ) do
-						print( key, value:to_table() )
+				-- dst
+				local src, fuel = 1,2
+				local dst_list = {}
+				local temp_stack, total_items = nil, 0
+				for index = 1, inv:get_size( 'dst' ), 1 do
+					temp_stack = inv:get_stack( 'dst', index )
+					if not temp_stack:is_empty() then
+						total_items = total_items + temp_stack:get_free_space()
 					end
 				end
-				temp_stack.count = src_list_results['amount']
-				temp_stack = ItemStack( temp_stack )
-				inv:remove_item( 'src', temp_stack )
-				meta:set_int( 'src_time', os.time() )
+				total_items = ( tmp_list[src]['item']:get_stack_max() * inv:get_size( 'dst' ) ) - total_items
+				dst_list['free_space'] = total_items
+				-- Time to see how much results we cooked
+				if tmp_list[src]['total_time'] < tmp_list[fuel]['total_time'] and tmp_list[fuel].total_time ~= 0 then
+					-- we didn't had enough fuel to cook all items, correct value
+					tmp_list[src]['total_time'] = tmp_list[fuel]['total_time']
+					tmp_list[src]['amount'] = math.floor( tmp_list[src]['total_time'] / tmp_list[src]['time'] )
+				else
+					-- there was more fuel than needed
+					tmp_list[fuel]['total_time'] = tmp_list[src]['total_time']
+					tmp_list[fuel]['amount'] = math.floor( tmp_list[fuel]['total_time'] / tmp_list[fuel]['time'] )
+				end
+				if dst_list.free_space < ( tmp_list[src]['amount'] * tmp_list[src]['item']:get_count() ) then
+					-- we also have to correct the free space amount -_-
+					tmp_list[src]['amount'] = math.floor( total_items / tmp_list[src]['item']:get_count() )
+					tmp_list[src]['total_time'] = tmp_list[src]['amount'] * tmp_list[src]['time']
+					tmp_list[fuel]['total_time'] = tmp_list[src]['total_time']
+					tmp_list[fuel]['amount'] = tmp_list[fuel]['total_time'] * tmp_list[fuel]['time']
+					meta:set_int( 'cooking_state', 0 )
+					print( 'Switched to inactive state...4' )
+				end
+				if ( src_diff > tmp_list[src]['time'] ) then
+					temp_stack, total_items = nil, 0
+					total_items = tmp_list[src]['amount']
+					temp_stack = tmp_list[src]['item']:to_table()
+					temp_stack.count = total_items
+					temp_stack = ItemStack( temp_stack )
+					inv:add_item( 'dst', temp_stack )
+					temp_stack = inv:get_stack( 'src', src_index ):to_table()
+					temp_stack.count = tmp_list[src]['amount']
+					temp_stack = ItemStack( temp_stack )
+					inv:remove_item( 'src', temp_stack )
+					meta:set_int( 'src_time', os.time() )
+					src_diff = src_diff - tmp_list[src]['total_time']
+				end
+				if ( fuel_diff > tmp_list[fuel]['time'] ) then
+					temp_stack = inv:get_stack( 'fuel', fuel_index ):to_table()
+					if not temp_stack == nil then
+						temp_stack.count = tmp_list[fuel]['amount'] - 1
+						temp_stack = ItemStack( temp_stack )
+						inv:remove_item( 'fuel', temp_stack )
+						fuel_diff = fuel_diff - tmp_list[fuel]['total_time']
+					end
+					meta:set_int( 'fuel_time' , os.time() )
+				end
+				if fuel_diff == 0 or src_diff == 0 then
+					loop_trigger = false
+					break
+				elseif security_trigger >= 5 then
+					loop_trigger = false
+				end
+				security_trigger = security_trigger + 1
 			end
-			if ( fuel_diff > fuel.time ) then
-				temp_stack = fuel.item:to_table()
-				temp_stack.count = fuel_list_results['amount'] - 1
-				temp_stack = ItemStack( temp_stack )
-				inv:remove_item( 'fuel', temp_stack )
-				meta:set_int( 'fuel_time' , os.time() )
-			end
-			local pos_string = pos.x..','..pos.y..','..pos.z
-			local formspec = "size[9,9]"..
-				"image[3,3;1,1;default_furnace_fire_bg.png^default_furnace_fire_fg.png]"..
-				"list[nodemeta:"..pos_string..";fuel;1,4;2,2;]"..
-				"list[nodemeta:"..pos_string..";src;1,1;2,2;]"..
-				"list[nodemeta:"..pos_string..";dst;5,1;4,4;]"..
-				"list[current_player;main;0,6;8,4;]"
-			minetest.show_formspec(  clicker:get_player_name() , 'tb_furnace:unused', formspec )
+			show_formspec( pos, clicker:get_player_name(), 2 )
 		end
 	end,
 	on_construct = function(pos)
